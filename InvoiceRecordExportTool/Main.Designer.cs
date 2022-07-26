e9d68d72d444325ba385fd1e38bfff753563bf36
Lewis <Lewis@DESKTOP-UC56A9R>
2022-07-26 20:08:33 +0800

    添加项目文件。

---
+++ InvoiceRecordExportTool/Main.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace InvoiceRecordExportTool
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Mean = new System.Windows.Forms.MenuStrip();
+            this.tmUpdate = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmCustomer = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tmMaterial = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btngenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Mean.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Mean
+            // 
+            this.Mean.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Mean.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tmUpdate,
+            this.tmClose});
+            this.Mean.Location = new System.Drawing.Point(0, 0);
+            this.Mean.Name = "Mean";
+            this.Mean.Size = new System.Drawing.Size(270, 25);
+            this.Mean.TabIndex = 0;
+            this.Mean.Text = "menuStrip1";
+            // 
+            // tmUpdate
+            // 
+            this.tmUpdate.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tmCustomer,
+            this.toolStripSeparator1,
+            this.tmMaterial});
+            this.tmUpdate.Name = "tmUpdate";
+            this.tmUpdate.Size = new System.Drawing.Size(92, 21);
+            this.tmUpdate.Text = "更新基础信息";
+            // 
+            // tmCustomer
+            // 
+            this.tmCustomer.Name = "tmCustomer";
+            this.tmCustomer.Size = new System.Drawing.Size(172, 22);
+            this.tmCustomer.Text = "客户基础信息列表";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(169, 6);
+            // 
+            // tmMaterial
+            // 
+            this.tmMaterial.Name = "tmMaterial";
+            this.tmMaterial.Size = new System.Drawing.Size(172, 22);
+            this.tmMaterial.Text = "物料对应分类编码";
+            // 
+            // tmClose
+            // 
+            this.tmClose.Name = "tmClose";
+            this.tmClose.Size = new System.Drawing.Size(44, 21);
+            this.tmClose.Text = "关闭";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btngenerate);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(270, 275);
+            this.panel1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 246);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "查询";
+            // 
+            // btngenerate
+            // 
+            this.btngenerate.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btngenerate.Location = new System.Drawing.Point(0, 252);
+            this.btngenerate.Name = "btngenerate";
+            this.btngenerate.Size = new System.Drawing.Size(270, 23);
+            this.btngenerate.TabIndex = 1;
+            this.btngenerate.Text = "运算";
+            this.btngenerate.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(11, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "应收单-单据日期";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "结束日期:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "开始日期:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(71, 68);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(170, 21);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(70, 100);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(170, 21);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(14, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 36);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "注意:查询的应收单为已下推\r\n\r\n \'销售增值税专用发票\'记录";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 300);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Mean);
+            this.MainMenuStrip = this.Mean;
+            this.Name = "Main";
+            this.Text = "开票上传数据导出工具";
+            this.Mean.ResumeLayout(false);
+            this.Mean.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip Mean;
+        private System.Windows.Forms.ToolStripMenuItem tmUpdate;
+        private System.Windows.Forms.ToolStripMenuItem tmCustomer;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem tmMaterial;
+        private System.Windows.Forms.ToolStripMenuItem tmClose;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btngenerate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
